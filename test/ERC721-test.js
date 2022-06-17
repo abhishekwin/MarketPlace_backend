f5a9f82d4fa2d98cc913190932e41d0d71b0af9b
Abhi <dr.mac@DrMacs-iMac.local>
2022-06-17 19:30:00 +0530

    writing test cases for token contract

--- test/ERC721-test.js
+++ test/ERC721-test.js
@@ -17,16 +17,33 @@ describe("MyNFT", () => {
 
   describe("Checking Royality", () => {
     it("Should check that Maximum Royalty is ", async () => {
+
       expect(await myNFT.maximumRoyality()).to.be.equals(500);
     });
   });
 
   describe("Cheking  mint function ", () => {
     it("Should check that NFT is mint ", async () => {
-      await myNFT.mint(deployer.address, "hello world", 300);
+      await myNFT.mint(deployer.address, "hello world", 400);
       expect(await myNFT.ownerOf(1)).to.be.equal(deployer.address);
     });
   });
+
+  describe("Cheking  mint function ", () => {
+    it("Should check that royality", async () => {
+      
+     await expect(myNFT.mint(deployer.address, "hello world", 600)).to.be.revertedWith('Royality should be less');
+    });
+  });
+
+  describe("Cheking  mint function ", () => {
+    it("Should check that Address", async () => {
+      
+     await expect(myNFT.mint("0x0000000000000000000000000000000000000000", "hello world", 400)).to.be.revertedWith("Token contract: to address can't be 0x0");
+    });
+  });
+
+  
 
   describe("Checking that transferFrom function is working or not", () => {
     it("Should transfer token betwen accounts", async () => {
@@ -38,6 +55,19 @@ describe("MyNFT", () => {
       // expect( await myNFT.balanceOf(add1balance)).to.be.equal(1);
     });
   });
+
+  // describe("Checkinhg transfer negative", () =>{
+  //   it("Should check the token is transfer or not", async ()=>{
+  //     await expect(myNFT.transferFrom(deployer.address,add1.address,1)).to.be.revertedWith("ERC721: transfer caller is not owner napproved");
+  //   });
+  // });
+
+
+
+ 
+
+
+
   describe("Checking that balanceOf function", () => {
     it("Should return balance of the acoount", async () => {
       const abc = await myNFT.balanceOf(add1.address);
@@ -46,6 +76,8 @@ describe("MyNFT", () => {
     });
   });
 
+ 
+
   describe("Checking that approve function", () => {
     it("Should approve the account", async () => {
       await myNFT.connect(add1).approve(add2.address, 1);
@@ -53,7 +85,16 @@ describe("MyNFT", () => {
       expect(await myNFT.getApproved(1)).to.be.equal(add2.address);
     });
   });
+
+  describe("Checking that negative type", () => {
+    it("Should check account not approve", async () => {
+      await expect (myNFT.approve(add1.address,1)).to.be.revertedWith('ERC721: approval to current owner')
+    });
+  });
+
   
+  
+
   describe("Checking that setApprovalForAll function", () => {
     it("Should Approved for all", async () => {
       await myNFT.setApprovalForAll(add2.address, true);
@@ -62,4 +103,6 @@ describe("MyNFT", () => {
       ).to.be.equal(true);
     });
   });
+
+ 
 });
