cc4dda6dfede8152b64243806e5539cfe183bfeb
sanjay <90381993+deepakios-thoughtwin@users.noreply.github.com>
2022-07-07 13:30:28 +0530

    fix issues conflict

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-require('@nomiclabs/hardhat-waffle');
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-=======
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require('@openzeppelin/hardhat-upgrades');
@@ -12,19 +5,14 @@ require('@openzeppelin/hardhat-upgrades');
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
->>>>>>> 1f47f51acdc4691060787c03209b4f4e66364eaa
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
   }
 });
-<<<<<<< HEAD
-
-=======
 const Private_Key = "13b110e7ac194d2827d04f17c11726068bb2dc19b595b5a33acaf7fc15604e1b"
 const rinkeby_api_key = "K63V19BYNUEP2EEIKIZYE1CGWY85SCRF41"
->>>>>>> 1f47f51acdc4691060787c03209b4f4e66364eaa
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -32,40 +20,15 @@ const rinkeby_api_key = "K63V19BYNUEP2EEIKIZYE1CGWY85SCRF41"
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-<<<<<<< HEAD
-  solidity: {
-    compilers: [
-      {
-        version: "0.4.18",
-        settings: {
-          optimizer: {
-            enabled: false,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: "0.8.14",
-        settings: {
-          optimizer: {
-            enabled: false,
-            runs: 200,
-          },
-        },
-      },
-    ]
-  }
-}
-=======
- 
+
 
 
   solidity: "0.8.4",
   networks: {
-  	rinkeby: {
-  		url: `https://rinkeby.infura.io/v3/328ff9ac7ccf4b8b98de2b55b4047bd6`,
-  		accounts: [`0x${Private_Key}`]
-  	},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/328ff9ac7ccf4b8b98de2b55b4047bd6`,
+      accounts: [`0x${Private_Key}`]
+    },
   },
   etherscan: {
     apiKey: {
@@ -74,9 +37,8 @@ module.exports = {
   }
 }
 
-  
+
 
 //0xfd67a1a3757b296c1ac016254f3be36adaaba6f6  ropsten mytoken proxy per
 // 0x57A5BC0ec9a36F2cf6d9d9e3e01133cF5BD33316  rinkeby
 // 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0   localhost
->>>>>>> 1f47f51acdc4691060787c03209b4f4e66364eaa
